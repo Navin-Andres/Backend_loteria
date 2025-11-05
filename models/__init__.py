7afed23cd2e16a4a2c7ce2da9cd05eb357ee6114
unknown <navinbalmaceda83@gmail.com>
2025-11-05 16:30:34 -0500

    Fix: Add model imports in __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+from .lottery import LotteryResult, FrequentNumber, Statistics
+from .session import Session
+from .user import User
+
+__all__ = ['db', 'LotteryResult', 'FrequentNumber', 'Statistics', 'Session', 'User']
